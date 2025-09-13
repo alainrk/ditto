532d8fb56b171ac32b27b7ab5c465eec0a146cd0
Alain <alain.devcs@gmail.com>
2025-09-13 04:35:57 +0200

    flags for raw mode

--- src/main.c
+++ src/main.c
@@ -14,17 +14,34 @@ void enableRawMode(void) {
 
   struct termios raw = orig_termios;
 
-  // Disable:
+  // Disabling some Input flags:
+  // - Break condition causing sigint
   // - Signal ctrl-m (fix specific)
+  // - Parity check (old)
+  // - 8th bit each input being stripped out
   // - Signals flow control (ctrl-s, ctrl-q)
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+  // Disabling some Output flags:
   // - Output processing (\n, \r\n) -> must add \r ourself now
   raw.c_oflag &= ~(OPOST);
+
+  // Disabling some Local flags:
   // - Echoing
   // - Canonical (byte by byte read) mode
   // - Signals Ctrl-v (wait input), ctrl-o fix in macos
   // - Signals int and tstp (ctrl-c, ctrl-z)
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  // Disabling some Control flags:
+  // - Old stuff
+  raw.c_cflag &= ~(CS8);
+
+  // Setting some Control Characters:
+  // read() witll return as soon as any byte is read
+  raw.c_cc[VMIN] = 0;
+  // max amount of time to wait before read() returns (tenths of a second)
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -32,13 +49,16 @@ void enableRawMode(void) {
 int main(void) {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q')
+      break;
   }
 
   return 0;
