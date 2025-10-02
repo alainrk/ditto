a357e6fbd36aead319a6d4d63f54e660476132d1
Alain <alain.devcs@gmail.com>
2025-10-02 05:32:58 +0200

    implementing backspace - fix

--- src/main.c
+++ src/main.c
@@ -542,8 +542,9 @@ void editorRowDeleteChar(Row *row, uint32_t at) {
   if (at < 0 || at >= row->size)
     return;
   memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
   editorUpdateRow(row);
-  E.dirty--;
+  E.dirty++;
 }
 
 void editorDeleteChar(void) {
