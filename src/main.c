5e58458cb9b6f1c0766fe1d91124d4f25e7ad21a
Alain <alain.devcs@gmail.com>
2025-10-03 05:27:24 +0200

    add prompting for user command and similar

--- src/main.c
+++ src/main.c
@@ -856,6 +856,34 @@ void editorRefreshScreen(void) {
 
 /*** input ***/
 
+char *editorPrompt(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+
+  size_t buflen = 0;
+  buf[0] = '\0';
+
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) { // Is printable character
+      if (buflen == bufsize - 1) {       // Exceeded buffer, need to double it
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
+
 void editorChangeMode(enum editorMode mode) { E.mode = mode; }
 
 void editorMoveCursor(int key) {
